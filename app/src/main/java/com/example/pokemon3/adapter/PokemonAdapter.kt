b810f6a62b93c6d91058e1a8becd6fe177685258
jonathanc <kozyo_lakoud@hotmail.com>
2024-03-04 09:00:41 -0600

    - Recycler view -Main Activity findView por Binding

--- app/src/main/java/com/example/pokemon3/adapter/PokemonAdapter.kt
+++ app/src/main/java/com/example/pokemon3/adapter/PokemonAdapter.kt
@@ -7,12 +7,14 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.pokemon3.R
 import com.example.pokemon3.data.Pokemon
+import com.example.pokemon3.MainActivity
+import com.example.pokemon3.databinding.ItemPokemonBinding
 
 class PokemonAdapter(private val pokemonList:List<Pokemon>) : RecyclerView.Adapter<PokemonViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PokemonViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        return PokemonViewHolder(layoutInflater.inflate(R.layout.item_pokemon, parent, false))
+        val binding = ItemPokemonBinding.inflate(LayoutInflater.from(parent.context))
+        return PokemonViewHolder(binding)
     }
 
     override fun getItemCount(): Int = pokemonList.size
@@ -23,9 +25,12 @@ class PokemonAdapter(private val pokemonList:List<Pokemon>) : RecyclerView.Adapt
     }
 }
 
-class PokemonViewHolder(view: View):RecyclerView.ViewHolder(view) {
+//class PokemonViewHolder(view: View):RecyclerView.ViewHolder(view)
+class PokemonViewHolder(private val binding: ItemPokemonBinding) : RecyclerView.ViewHolder(binding.root)
+{
 
-    val pokemon = view.findViewById<TextView>(R.id.textView)
+
+    val pokemon = binding.textView
     fun render(pokemonModel: Pokemon){
         pokemon.text = pokemonModel.name
     }
