d949f2ca324a6d5da6b505a6bfbf433c90486771
jonathanc <kozyo_lakoud@hotmail.com>
2024-03-18 23:05:43 -0600

    + MVVM

--- app/src/main/java/com/example/pokemon3/data/Api.kt
+++ app/src/main/java/com/example/pokemon3/data/Api.kt
@@ -1,11 +1,15 @@
 package com.example.pokemon3.data
 
+import com.example.pokemon3.data.models.Pokemon
+import com.example.pokemon3.data.models.PokemonDetailsModel
 import com.example.pokemon3.data.models.PokemonResponse
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
+import retrofit2.http.Url
 
 object Api {
 
@@ -16,8 +20,17 @@ object Api {
     interface RemoteService{
         @GET("pokemon?limit=151")
         fun loadPokemon(@Query("limit") limit:Int) : Call<PokemonResponse>
+        //La agregue para obtener el Id del pokemon
+        @GET
+        fun getPokemonInfo(@Url url: String): Call<PokemonDetailsModel>
     }
 
+    // 1.- Agregar funcion que va a consumir la nueva URL
+
+//    fun buildtwo(url: String) : RemoteService {
+//        val retrofit = builder.baseUrl(url).build()
+//        return retrofit.create(RemoteService::class.java)
+//    }
     fun build() : RemoteService{
         return builder.build().create(RemoteService::class.java)
     }
